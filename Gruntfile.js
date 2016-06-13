5a48bf427191e007a5a89c21137ac3d3d546931f
JoeChildress <joe.ch@comcast.net>
2016-06-12 20:00:04 -0700

    updates to Gruntfile.js and .eslintrc.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,29 +1,22 @@
 module.exports = function (grunt) {
-  
-  //add a watch task or plugin to update when you make file changes. Not in this version
-  
-  //define how plugins work
+
   grunt.initConfig({
-    //template compiler
     jst: {
       compile: {
-        options: {//compiles all of the templates into one file
+        options: {
           namespace: 'TD',
           processName: function (filepath) {
-            //changing the path names
             return filepath.replace(/(public\/js\/templates\/|\.tpl)/ig, '');
           }
         },
         files: {
-          //take everything from templates and compile into templates.js file
           'public/js/templates/templates.js': 'public/js/templates/*.tpl'
         }
       }
     },
-    //bundles the code for the browswer to use
     browserify: {
       app: {
-        src: 'public/js/main.js',//start file for browserify
+        src: 'public/js/main.js',
         dest: 'public/js/main.bundled.js'
       }
     },
@@ -37,6 +30,6 @@ module.exports = function (grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-jst');
   grunt.loadNpmTasks('grunt-browserify');
-  grunt.registerTask('default', ['jst', 'browserify']);//default code to run with grunt
+  grunt.registerTask('default', ['jst', 'browserify']);
   grunt.loadNpmTasks('grunt-contrib-watch');
 };
