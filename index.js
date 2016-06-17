3f5acc0712e03ee0c98157d8e21db96f5801dda8
Katja Borchert <katjab@uw.edu>
2016-06-16 22:30:25 -0700

    updated index.js and index.html

--- index.js
+++ index.js
@@ -10,14 +10,15 @@ var db = mongoose.connect('mongodb://localhost/houseAPI');
 /*eslint-enable no-unused-vars*/
 
 var app = express();
+var port = process.env.PORT || 3000;
 var root = path.resolve(__dirname, 'public');
 
 app.use(express.static(root));
 app.use('/api/houses', houseAPIRouter);
 app.use(spaRoute(path.resolve(root, 'index.html')));
 
-app.listen(3000, function () {
+app.listen(port, function () {
   /* eslint-disable no-console */
-  console.log('listening on port 3000');
+  console.log('listening on port', port);
   /* eslint-enable no-console */
 });
